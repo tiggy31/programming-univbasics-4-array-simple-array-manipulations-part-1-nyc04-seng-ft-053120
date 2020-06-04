11da8a543ae19e41eadc4757f45136cf44bc40d5
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-04 12:06:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_push(array,string)
   array.push(string)
 end
 
-def using_unshift(Array,string)
+def using_unshift(array,string)
   
-  Array.unshift(string)
+  array.unshift(string)
   
 end
